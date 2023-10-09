37e576bac6977dbf8fd285ed32c8700f2de30d00
kennethoyahebholo <oyahebholokennethizosea@gmail.com>
2023-10-09 17:01:11 +0100

    feat: sign in

--- api/User.js
+++ api/User.js
@@ -102,11 +102,58 @@ router.post("/signup", (req, res) => {
 
 // sign in
 router.post("/signin", (req, res) => {
-  // let { name, email, password, dateOfBirth } = req.body
-  // name = name.trim();
-  // email = email.trim();
-  // password = password.trim();
-  // dateOfBirth = dateOfBirth.trim();
+  let { email, password } = req.body;
+  email = email.trim();
+  password = password.trim();
+
+  if (email === "" || password === "") {
+    res.json({
+      status: "FAILED",
+      message: "Empty Input Fields!",
+    });
+  } else {
+    User.find({ email })
+      .then((data) => {
+        if (data) {
+          // user exist
+          const hashedPassword = data[0].password;
+          bcrypt
+            .compare(password, hashedPassword)
+            .then((result) => {
+              if (result) {
+                // password match
+                res.json({
+                  status: "SUCCESS",
+                  message: "Sign in successful",
+                  data: data,
+                });
+              } else {
+                res.json({
+                  status: "FAILED",
+                  message: "Invalid password entered",
+                });
+              }
+            })
+            .catch((err) => {
+              res.json({
+                status: "FAILED",
+                message: "An error occurred while comparing passwords",
+              });
+            });
+        } else {
+          res.json({
+            status: "FAILED",
+            message: "Invalid credentials entered",
+          });
+        }
+      })
+      .catch((err) => {
+        res.json({
+          status: "FAILED",
+          message: "An error occurred while checking for existing user",
+        });
+      });
+  }
 });
 
 module.exports = router;
