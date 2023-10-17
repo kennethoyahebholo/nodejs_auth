f32fc3f1123098ee72246b67cc1ccb59f3646c58
kennethoyahebholo <oyahebholokennethizosea@gmail.com>
2023-10-17 18:30:16 +0100

    fix: bug

--- api/User.js
+++ api/User.js
@@ -160,7 +160,7 @@ router.post("/signup", (req, res) => {
 // send verification
 const sendVerificationEmail = ({ _id, email }, res) => {
   // url to be used in the email
-  const currentUrl = "http://localhost:5000/";
+  const currentUrl = process.env.BASE_URL || "http://localhost:5000/";
 
   const uniqueString = uuidv4() + _id;
 
