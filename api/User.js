99fd29ecfae3c715319c7128782b2d16aad77ad1
kennethoyahebholo <oyahebholokennethizosea@gmail.com>
2023-10-27 18:41:27 +0100

    added env variable

--- api/User.js
+++ api/User.js
@@ -156,7 +156,7 @@ router.post("/signup", (req, res) => {
 // send verification
 const sendVerificationEmail = ({ _id, email }, res) => {
   // url to be used in the email
-  const currentUrl = process.env.BASE_URL;
+  const currentUrl = process.env.BASE_URL || "http://localhost:5000/";
 
   const uniqueString = uuidv4() + _id;
 
