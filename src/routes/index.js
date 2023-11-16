0d8777b81aaee96ae0913cded98d116d2badf468
kennethoyahebholo <oyahebholokennethizosea@gmail.com>
2023-11-16 17:47:40 +0100

    code base revamp

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,7 +2,9 @@ const express = require("express");
 const router = express.Router();
 
 const userRoutes = require("./../domains/user");
+const forgetPasswordRoutes = require("./../domains/forgot_password");
+const userVerification = require("./../domains/user_verification");
 
-router.use("/user", userRoutes);
+router.use("/user", userRoutes, forgetPasswordRoutes, userVerification);
 
 module.exports = router;
