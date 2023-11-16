35481841a182a1b3d3e335facd6fdb492d0b9c54
kennethoyahebholo <oyahebholokennethizosea@gmail.com>
2023-11-16 21:09:21 +0100

    code base revamp

--- src/domains/forgot_password/routes.js
+++ src/domains/forgot_password/routes.js
@@ -6,6 +6,8 @@ const {
   handlePasswordReset,
 } = require("./controller");
 
+const { createUser, authenticateUser } = require("../user/controller");
+
 // Password Reset
 router.post("/requestPasswordReset", async (req, res) => {
   try {
@@ -26,6 +28,40 @@ router.post("/requestPasswordReset", async (req, res) => {
     res.json({
       status: "FAILED",
       message: "failed to reset password, haba",
+    });
+  }
+});
+
+// sign in
+router.post("/signin", async (req, res) => {
+  try {
+    let { email, password } = req.body;
+    email = email.trim();
+    password = password.trim();
+
+    if (email === "" || password === "") {
+      throw Error("Empty Input Fields!");
+    } else {
+      //authenticate the user
+      const authenticatedUser = await authenticateUser(email, password);
+      if (authenticatedUser) {
+        // password match
+        res.json({
+          status: "SUCCESS",
+          message: "Sign in successful",
+          data: authenticatedUser,
+        });
+      } else {
+        res.json({
+          status: "FAILED",
+          message: "Invalid password entered",
+        });
+      }
+    }
+  } catch (err) {
+    res.json({
+      status: "FAILED",
+      message: err.message,
     });
   }
 });
