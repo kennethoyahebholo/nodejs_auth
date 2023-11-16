6ccbd5d40cae6ef35c03033c90d195ff2e2c7909
kennethoyahebholo <oyahebholokennethizosea@gmail.com>
2023-11-16 20:09:30 +0100

    code base revamp

--- src/domains/forgot_password/routes.js
+++ src/domains/forgot_password/routes.js
@@ -12,36 +12,36 @@ const {
 router.post("/requestPasswordReset", async (req, res) => {
   try {
     let { email, redirectUrl } = req.body;
-    email = email.trim();
-    redirectUrl = phoneNumber.trim();
-    if (email === "" || redirectUrl === "") {
-      throw Error("Empty Input Fields!");
-    } else if (!/^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/.test(email)) {
-      throw Error("Invalid email entered");
-    } else {
-      // const resetString = uuidv4() + _id;
-      // const existingUser = await checkForExistingUser(email);
+    // email = email.trim();
+    // redirectUrl = phoneNumber.trim();
+    // if (email === "" || redirectUrl === "") {
+    //   throw Error("Empty Input Fields!");
+    // } else if (!/^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/.test(email)) {
+    //   throw Error("Invalid email entered");
+    // } else {
+    const resetString = uuidv4() + _id;
+    // const existingUser = await checkForExistingUser(email);
 
-      // if (!existingUser.isVerified) {
-      //   res.json({
-      //     status: "FAILED",
-      //     message: "Email hasn't been verified yet, check you inbox",
-      //   });
-      // } else {
-      //   await sendResetEmail(existingUser[0], redirectUrl, resetString);
-      //   res.json({
-      //     status: "PENDING",
-      //     message: "Password reset email sent",
-      //   });
-      // }
-      res.json({
-        status: "PENDING",
-        message: "Password reset email sent",
-        email,
-        redirectUrl,
-        // id: resetString,
-      });
-    }
+    // if (!existingUser.isVerified) {
+    //   res.json({
+    //     status: "FAILED",
+    //     message: "Email hasn't been verified yet, check you inbox",
+    //   });
+    // } else {
+    //   await sendResetEmail(existingUser[0], redirectUrl, resetString);
+    //   res.json({
+    //     status: "PENDING",
+    //     message: "Password reset email sent",
+    //   });
+    // }
+    res.json({
+      status: "PENDING",
+      message: "Password reset email sent",
+      email,
+      redirectUrl,
+      id: resetString,
+    });
+    // }
   } catch (err) {
     res.json({
       status: "FAILED",
