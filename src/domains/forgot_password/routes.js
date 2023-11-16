b9f882d8fbd07620ccc5708486bbffea02fd002c
kennethoyahebholo <oyahebholokennethizosea@gmail.com>
2023-11-16 21:16:57 +0100

    code base revamp

--- src/domains/forgot_password/routes.js
+++ src/domains/forgot_password/routes.js
@@ -6,14 +6,38 @@ const {
   handlePasswordReset,
 } = require("./controller");
 
-const { createUser, authenticateUser } = require("../user/controller");
+// const { createUser, authenticateUser } = require("../user/controller");
 
 // Password Reset
+// router.post("/requestPasswordReset", async (req, res) => {
+//   try {
+//     let { email, redirectUrl } = req.body;
+//     email = email.trim();
+//     redirectUrl = phoneNumber.trim();
+
+//     if (email === "" || redirectUrl === "") {
+//       throw Error("Empty Input Fields!");
+//     } else {
+//       await sendEmailForPasswordReset(email, redirectUrl);
+//       res.json({
+//         status: "PENDING",
+//         message: "Password reset email sent",
+//       });
+//     }
+//   } catch (err) {
+//     res.json({
+//       status: "FAILED",
+//       message: "failed to reset password, haba",
+//     });
+//   }
+// });
+
+// // sign in
 router.post("/requestPasswordReset", async (req, res) => {
   try {
     let { email, redirectUrl } = req.body;
     email = email.trim();
-    redirectUrl = phoneNumber.trim();
+    redirectUrl = redirectUrl.trim();
 
     if (email === "" || redirectUrl === "") {
       throw Error("Empty Input Fields!");
@@ -23,40 +47,6 @@ router.post("/requestPasswordReset", async (req, res) => {
         status: "PENDING",
         message: "Password reset email sent",
       });
-    }
-  } catch (err) {
-    res.json({
-      status: "FAILED",
-      message: "failed to reset password, haba",
-    });
-  }
-});
-
-// sign in
-router.post("/signin", async (req, res) => {
-  try {
-    let { email, password } = req.body;
-    email = email.trim();
-    password = password.trim();
-
-    if (email === "" || password === "") {
-      throw Error("Empty Input Fields!");
-    } else {
-      //authenticate the user
-      const authenticatedUser = await authenticateUser(email, password);
-      if (authenticatedUser) {
-        // password match
-        res.json({
-          status: "SUCCESS",
-          message: "Sign in successful",
-          data: authenticatedUser,
-        });
-      } else {
-        res.json({
-          status: "FAILED",
-          message: "Invalid password entered",
-        });
-      }
     }
   } catch (err) {
     res.json({
